4adb8aeb5a945deef6e319667dc5b377e98fcc01
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-08-19 20:59:48 -0300

    updating global css and app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from './components/NavBar/NavBar.jsx'
 import Home from './components/Home/Home.jsx'
-import { About } from './components/About/About.jsx'
+import About from './components/About/About.jsx'
 
 function App() {
 

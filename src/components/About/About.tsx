f044a80d6f98847eaffd8ad1119457578eac3858
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-09-01 10:34:58 -0300

    adding dark theme in about component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-scroll';
 import { ProfileCard } from './ProfileCard/ProfileCard';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useTheme } from '../../hooks/useTheme';
 
 const About: React.FC = () => {
 
@@ -10,30 +11,33 @@ const About: React.FC = () => {
     AOS.init();
   }, []);
 
+  const { theme, setTheme} = useTheme()
+
   return (
-    <div id='about' className='w-full flex items-center justify-center md:py-16 sm:py-14 xs:py-7 xl:px-[180px] md:px-[150px] sm:px-[100px] 
-    xs:px-[50px] font-custom '>
+    <div id='about' className={`w-full flex items-center justify-center md:py-16 sm:py-14 xs:py-7 xl:px-[180px] md:px-[150px] sm:px-[100px] 
+    xs:px-[50px] font-custom ${theme === 'light' ? "text-light-secondary" : "text-dark-secondary"}`}>
       <div className='max-w-[1240px] m-auto grid xl:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 xs:grid-cols-1 xl:gap-8 md:gap-0 md:pt-[50px] 
       sm:pt-[20px] xs:pt-[10px]' >
         <div className='col-span-2'>  
-          <p className=' text-3xl tracking-widest text-[#8685d4] font-bold font-custom'>
+          <p className={`text-3xl tracking-widest font-bold font-custom 
+          ${theme === 'light' ? "text-light-secondary" : "text-dark-textNavbar"}`}>
             Sobre
           </p>
-          <h2 className='py-4 font-bold text-[#2c2b2c] md:text-[17px]  '>Quem eu sou</h2>
-          <p className='py-2 text-[#2c2b2c] md:text-[17px] xs:text-[16px]  text-justify' >
+          <h2 className='py-4 font-bold md:text-[17px]  '>Quem eu sou</h2>
+          <p className='py-2 md:text-[17px] xs:text-[16px]  text-justify' >
             Sou estudante de Sistemas de Informação no Centro de Informática da Universidade Federal de Pernambuco,
             atualmente na fase final do meu 4º semestre. Moro em Recife-PE e invisto meu tempo estudando diversas
             tecnologias para aprimorar minhas habilidades técnicas e não me limitar apenas aos conhecimentos proporcionados pela universidade.
           </p>
-          <p className='py-2 text-[#2c2b2c] md:text-[17px] xs:text-[16px] text-justify' >
+          <p className='py-2 ] md:text-[17px] xs:text-[16px] text-justify' >
             Possuo compreensão dos princípios de Algoritmos e Estruturas de Dados, Programação Orientada a Objetos,
              Redes de Computadores e Banco de Dados. Além disso, me dedico ao estudo de práticas como clean
              code, BDD, TDD e testes automatizados. Tenho domínio sobre as linguagens de programação Javascript e Python.
              Além dessas, possuo habilidades com outras tecnologias que podem ser revisadas na <span className='underline cursor-pointer'><Link to='skills'>seção de habilidades.</Link></span>
           </p>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400  rounded-xl flex items-center
-         justify-center p-4 md:mt-0 sm:mt-0 xs:mt-[30px]'>
+        <div className={`w-full h-auto m-auto shadow-md rounded-xl flex items-center
+         justify-center p-4 md:mt-0 sm:mt-0 xs:mt-[30px]  ${theme === 'light' ? "shadow-gray-500 " : "bg-[#353534] shadow-black"}`}>
           <ProfileCard />
         </div>
       </div>
--- src/components/About/ProfileCard/ProfileCard.tsx
+++ src/components/About/ProfileCard/ProfileCard.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import photo from '../../../assets/photo_profile.png';
 import { IoLogoGithub, IoLogoWhatsapp, IoLogoLinkedin } from 'react-icons/io';
+import { useTheme } from '../../../hooks/useTheme';
 
 export const ProfileCard: React.FC = () => {
 
   const linkZap = 'https://api.whatsapp.com/send?phone=5581989054991&text=Ol%C3%A1%20Matheus,%20Bom%20dia!';
 
+  const { theme, setTheme} = useTheme()
+
   return (
-    <div className=" w-full shadow-xl rounded-lg text-[#2c2b2c] ">
-      <div className="rounded-t-lg h-32 overflow-hidden ">
-        <img className="object-cover object-top w-full" src='https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Mountain'/>
+    <div className={`w-full rounded-lg  ${theme === 'light' ? "text-light-secondary" : "bg-dark-primary text-dark-textNavbar"}`}>
+      <div className="rounded-t-lg h-32 overflow-hidden bg-dark-primary">
+        {/* <img className="object-cover object-top w-full" src='https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Mountain'/> */}
       </div>
-      <div className="mx-auto w-[135px] h-[135px] relative -mt-16 border-4 border-white  bg-[#EFE7EB] rounded-full overflow-hidden">
-        <img className="object-cover object-center h-32" src={photo} alt='Woman looking front'/>
+      <div className={`mx-auto w-[135px] h-[135px] relative -mt-16 border-4 rounded-full overflow-hidden
+      ${theme === 'light' ? "bg-light-primary border-light-primary" : "bg-dark-image border-dark-image"}`}>
+        <img className="object-cover object-center h-36" src={photo} alt='matheus'/>
       </div>
       <div className="text-center mt-2">
         <h2 className="font-semibold">Matheus Malta</h2>
-        <p className="text-gray-500">Desenvolvedor</p>
+        <p className={`${theme === 'light' ? "text-light-secondary" : "text-dark-secondary"}`}>Desenvolvedor</p>
       </div>
-      <ul className="py-4 mt-2 text-gray-700 flex items-center justify-evenly">
+      <ul className={`py-4 mt-2 flex items-center justify-evenly ${theme === 'light' ? "text-light-secondary" : "text-dark-textNavbar"}`}>
         <li className="flex flex-col items-center justify-around px-0 ml-0">
-          <IoLogoGithub className='text-[#4d464c] w-[35px] h-[35px]' />
+          <IoLogoGithub className='w-[35px] h-[35px]' />
         </li>
         <li className="flex flex-col items-center justify-between px-0 ml-0">
-          <IoLogoLinkedin className='text-[#385bb9] w-[35px] h-[35px]' />
+          <IoLogoLinkedin className='w-[35px] h-[35px]' />
         </li>
         <li className="flex flex-col items-center justify-around px-0 ml-0">
-          <a href={linkZap}><IoLogoWhatsapp className='text-[#6cdd50] w-[35px] h-[35px]' /></a>
+          <a href={linkZap}><IoLogoWhatsapp className=' w-[35px] h-[35px]' /></a>
         </li>
       </ul>
-      <div className="p-4 border-t mx-8 mt-2">
+      <div className={`p-4 border-t mx-8 mt-2 ${theme === 'light' ? "border-light-secondary" : "border-[#353534]"}`}>
         <div className=' flex items-center justify-center'>
-          <button className='rounded-[10px] bg-[#8685d4] p-[10px] hover:scale-105 ease-in duration-300 
-          font-medium text-[#EFE7EB] md:w-auto sm:w-auto xs:w-full'>
+          <button className={`rounded-[10px] p-[10px] hover:scale-105 ease-in duration-300 font-medium
+          md:w-auto sm:w-auto xs:w-full ${theme === 'light' ? "bg-light-secondary text-light-primary" : "bg-dark-textNavbar text-dark-primary"}`}>
             Baixar Currículo
           </button>
         </div>
